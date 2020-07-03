e7016a70185390b803d16534f21fc88eb51b0a12
mrvnre <mrvnre24@gmail.com>
2020-07-03 11:24:46 -0500

    Created ab Abstract Entity

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -7,11 +7,11 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
-public class EventCategory {
+public class EventCategory extends AbstractEntity {
 
-  @Id
-  @GeneratedValue
-  private int id;
+//  @Id
+//  @GeneratedValue
+//  private int id;
 
   @Size(min = 3, message="Name must be at least 3 characters long")
   private String name;
@@ -26,23 +26,24 @@ public class EventCategory {
 
   public void setName(String name) { this.name = name; }
 
-  public int getId() {
-    return id;
-  }
+//  public int getId() {
+//    return id;
+//  }
 
   @Override
   public String toString() {
     return name;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    EventCategory that = (EventCategory) o;
-    return id == that.id;
-  }
+//  @Override
+//  public boolean equals(Object o) {
+//    if (this == o) return true;
+//    if (o == null || getClass() != o.getClass()) return false;
+//    EventCategory that = (EventCategory) o;
+//    return id == that.id;
+//  }
 
-  @Override
-  public int hashCode() { return Objects.hash(id); }
+//  @Override
+//  public int hashCode() { return Objects.hash(id); }
+
 }
